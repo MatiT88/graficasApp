fed5e0b19f06acd4f552a5359a88405893b57d80
MatiT88 <matiastecici@gmail.com>
2023-04-30 10:47:52 -0300

    se crearon todas las carpetas,modulos,components,etc

---
+++ src/app/graficas/components/graficas-barra/graficas-barra.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graficas-barra',
+  templateUrl: './graficas-barra.component.html',
+  styles: [
+  ]
+})
+export class GraficasBarraComponent {
+
+}
