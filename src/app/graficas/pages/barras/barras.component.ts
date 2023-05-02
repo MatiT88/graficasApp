23b6212673aa227852859f5b452a962330a581ef
MatiT88 <matiastecici@gmail.com>
2023-05-01 23:02:49 -0300

    todo listo hasta la parte de donas

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -1,11 +1,73 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+ 
 @Component({
   selector: 'app-barras',
   templateUrl: './barras.component.html',
-  styles: [
-  ]
 })
 export class BarrasComponent {
+ 
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+ 
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: true
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  public barChatLegend = true;
 
+ 
+  public barChartData: ChartData<'bar'> = {
+    labels: [ '2020', '2021', '2022', '2023', '2024', '2025', '2026' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#873e23', hoverBackgroundColor: 'red' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#154c79' },
+      { data: [ 33, 55, 66, 77, 88, 37, 48 ], label: 'Series C', backgroundColor: '#ebb678' },
+    ]
+  };
+ 
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100) ];
+
+    this.barChartData.datasets[1].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100) ];
+
+    this.barChartData.datasets[2].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100) ];
+
+    this.chart?.update();
+  }
+ 
 }
